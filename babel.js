e9f0e105b53302ebb185d05ffce104d24972598c
Nikola Pajkanović <npajkano@gmail.com>
2022-07-11 04:07:45 +0200

    commit new
    updated

---
+++ babel.js
@@ -0,0 +1,39 @@
+"use strict";
+
+$(document).ready(function () {
+  console.log("ready!");
+});
+$(window).on("load", function () {
+  let timeoutl;
+  let timeoutr;
+  $('.lftbtn').on("click", function () {
+    $('.toprow').append($('.toprow span:first').hide().fadeIn(3000));
+    $('.botrow').append($('.botrow span:first').hide().fadeIn(3000));
+
+    if ($(".toprow:animated") || $(".botrow:animated")) {
+      $('.lftbtn').prop('disabled', true);
+      $('.rghtbtn').prop('disabled', true);
+      timeoutl = setTimeout(wait, 3000);
+    }
+  });
+  $('.rghtbtn').on("click", function () {
+    $('.toprow').prepend($('.toprow span:last').hide().fadeIn(3000));
+    $('.botrow').prepend($('.botrow span:last').hide().fadeIn(3000));
+    $('.toprow span:last').hide().fadeIn(3000);
+    /*.slideToggle(3000);*/
+
+    $('.botrow span:last').hide().fadeIn(3000);
+    /*.slideToggle(3000);*/
+
+    if ($(".toprow:animated") || $(".botrow:animated")) {
+      $('.rghtbtn').prop('disabled', true);
+      $('.lftbtn').prop('disabled', true);
+      timeoutr = setTimeout(wait, 3000);
+    }
+  });
+
+  function wait() {
+    $('.lftbtn').prop('disabled', false);
+    $('.rghtbtn').prop('disabled', false);
+  }
+});
